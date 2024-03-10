f6173e0abf22dfcfda1776427cba8d32f4ea92bf
Saad Awan <saadiawan09@gmail.com>
2024-03-10 13:46:51 -0700

    added new test comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import logger from "./config/logger.js";
 
 const port = process.env.PORT || 3001;
 
+// server is started here!!!
 app.listen(port, () => {
     logger.info(`Server is running on http://localhost:${port}`);
 });
